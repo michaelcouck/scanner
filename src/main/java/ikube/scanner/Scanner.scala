0c14822b5ea3d03960550fa3dbcde502b97846a1
michaelcouck <Caherl1ne>
2014-11-17 18:07:16 +0100

    Added some JavaDoc.

--- src/main/java/ikube/scanner/Scanner.scala
+++ src/main/java/ikube/scanner/Scanner.scala
@@ -1,8 +1,8 @@
 package ikube.scanner
 
-import java.net._
+import java.net.{InetSocketAddress, Socket, InetAddress}
 import java.util
-import java.util.concurrent.{Executors, TimeUnit}
+import java.util.concurrent.{TimeUnit, Executors}
 
 import org.apache.commons.lang.StringUtils
 import org.apache.commons.net.util.SubnetUtils
@@ -12,7 +12,9 @@ import scala.concurrent._
 import scala.concurrent.duration.Duration
 
 /**
- * Bla, bla, bla...
+ * This is a simple utility to scan a network and report what ports are open on the
+ * scanning range specified. It is multi threaded, however there is no retry, and very
+ * little exception handling.
  *
  * @author Michael Couck
  * @version 01.00
@@ -20,11 +22,24 @@ import scala.concurrent.duration.Duration
  */
 class Scanner {
 
-  val SEPARATOR_CHARACTERS = ",|; "
   val LOGGER = LoggerFactory.getLogger(this.getClass.getName)
 
+  /** Characters used for splitting the port range string. */
+  val SEPARATOR_CHARACTERS = ",|; "
+  /**
+   * We define an executor with a few mode threads as the built in one has too few for good performance in this case.
+   */
   implicit val ec = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(100))
 
+  /**
+   * This method will scan all ports on the entire range defined in the parameter list. The range
+   * is in the format "192.168.1.0/24". In this particular case the range will be 0-255 for the network
+   * segment part.
+   *
+   * @param addressRange the range of addresses to scan for ports on the network, in the format "192.168.1.0/24"
+   * @param timeout the timeout to check first for pinging the machines, then trying the individual ports
+   * @return the ip addresses and ports as a list, in the format "192.168.1.1:8080"
+   */
   def scan(addressRange: String, timeout: Integer): util.ArrayList[String] = {
     val portRange = {
       List.range(0, 65535).toArray map (_.toString)
@@ -32,11 +47,29 @@ class Scanner {
     scan(addressRange, portRange, timeout)
   }
 
+  /**
+   * This method will scan a network, the addresses specified in the format "192.168.1.1/24",
+   * for the port range specified in the parameter list.
+   *
+   * @param addressRange the range of addresses to scan for ports on the network, in the format "192.168.1.0/24"
+   * @param portRange the range of ports to scan, in the format "80,8080,3306,1521,..."
+   * @param timeout the timeout to check first for pinging the machines, then trying the individual ports
+   * @return the ip addresses and ports as a list, in the format "192.168.1.1:8080"
+   */
   def scan(addressRange: String, portRange: String, timeout: Integer): util.ArrayList[String] = {
     val ports = StringUtils.split(portRange, SEPARATOR_CHARACTERS)
     scan(addressRange, ports, timeout)
   }
 
+  /**
+   * This method will scan a network, the addresses specified in the format "192.168.1.1/24",
+   * for the port range specified in the parameter list.
+   *
+   * @param addressRange the range of addresses to scan for ports on the network, in the format "192.168.1.0/24"
+   * @param portRange the range of ports to scan, in the format "80,8080,3306,1521,..."
+   * @param timeout the timeout to check first for pinging the machines, then trying the individual ports
+   * @return the ip addresses and ports as a list, in the format "192.168.1.1:8080"
+   */
   def scan(addressRange: String, portRange: Array[String], timeout: Integer): util.ArrayList[String] = {
     val reachableAddresses = new util.ArrayList[String]()
     val subnetUtils = new SubnetUtils(addressRange)
@@ -46,7 +79,7 @@ class Scanner {
         if (inetAddress.isReachable(timeout)) {
           Future.traverse(portRange.toList)(port => Future(
             try {
-              val socket = new Socket()
+              val socket = new Socket()()
               try {
                 socket.connect(new InetSocketAddress(address, Integer.parseInt(port)), timeout)
                 reachableAddresses.add(address + ":" + port)
